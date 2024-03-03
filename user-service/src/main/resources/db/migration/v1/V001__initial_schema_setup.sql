0c2876dad94e4fe6d807d41f318ac83842e22606
afsar <contactafsar@gmail.com>
2024-03-02 23:26:23 -0500

    S1E3: Flyway added

---
+++ user-service/src/main/resources/db/migration/v1/V001__initial_schema_setup.sql
@@ -0,0 +1,11 @@
+create schema if not exists user_mgmt;
+
+CREATE TABLE user_mgmt.user_detail (
+  id BIGSERIAL PRIMARY KEY,
+  userName VARCHAR(20) NOT NULL,
+  password VARCHAR(120) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  status VARCHAR(20)
+);
+
+
