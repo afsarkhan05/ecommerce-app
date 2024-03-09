87d16eb869b9cf2403f5273638163c5c21bfcf6e
afsar <contactafsar@gmail.com>
2024-03-09 16:34:30 -0500

    optimized pom - individual module has as and when needed

--- auth-common/src/main/java/com/zubi/ecommerce/auth/common/filter/AuthTokenFilter.java
+++ auth-common/src/main/java/com/zubi/ecommerce/auth/common/filter/AuthTokenFilter.java
@@ -11,20 +11,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.security.Key;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
